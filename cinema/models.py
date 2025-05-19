39db170cc83c071a223e66fe1b542f105d4273cf
Timur Khvat <timurkhvat@gmail.com>
2025-05-19 16:26:52 +0200

    'Solution'

---
+++ cinema/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    duration = models.IntegerField()
+
+    class Meta:
+        verbose_name_plural = "movies"
+
+    def __str__(self):
+        return f"{self.title}: {self.duration} min"
